97c7bccbeb3f1deb9cb2d15514f81508821beb86
RealAngeleno <119266468+RealAngeleno@users.noreply.github.com>
2022-11-27 16:25:51 -0700

    Delete perllib directory

--- perllib/CGI/Session/Serialize/storable.pm
+++
@@ -1,1 +0,0 @@
-package CGI::Session::Serialize::storable;use strict;use Storable;require CGI::Session::ErrorHandler;$CGI::Session::Serialize::storable::VERSION='4.43';@CGI::Session::Serialize::storable::ISA=("CGI::Session::ErrorHandler");sub freeze{my($self,$data)=@_;return Storable::freeze($data);}sub thaw{my($self,$string)=@_;return Storable::thaw($string);}1;
