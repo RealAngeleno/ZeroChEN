b5a5057307c85cf117440445912b6ad9e6dce63c
RealAngeleno <119266468+RealAngeleno@users.noreply.github.com>
2022-11-29 20:16:47 -0700

    Update orald.pl

--- test/module/orald.pl
+++ test/module/orald.pl
@@ -207,10 +207,10 @@ HTML
 			
 			$Page->Print(<<HTML);
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html lang="ja">
+<html lang="en">
 <head>
 
-	<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 	<title>ERROR!</title>
 
